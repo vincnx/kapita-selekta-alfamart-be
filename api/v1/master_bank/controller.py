33215beacc7169481533190f527072fd6f574b7e
vincent exelcio <vincent.celcio@gmail.com>
2024-11-08 19:58:14 +0700

    fix: convert id to ObjectId to fix error

--- api/v1/master_bank/controller.py
+++ api/v1/master_bank/controller.py
@@ -1,5 +1,6 @@
 from datetime import datetime, UTC
-from pymongo.errors import DuplicateKeyError
+from bson import ObjectId
+from pymongo.errors import WriteError
 from common.db import dbInstance
 from common.helpers.types import TypeMasterBank, TypeMasterBankInput
 from flask import abort
@@ -20,9 +21,10 @@ def findAllMasterBank()->tuple[list[TypeMasterBank], int]:
 
 def findMasterBankById(masterBankId:str)->tuple[TypeMasterBank, int]:
     # check if master bank data exists
+    print(masterBankId)
     try:
         masterBankData = masterBankCollection.find_one({
-            '_id': masterBankId,
+            '_id': ObjectId(masterBankId),
             'activeStatus': True
         })
     except Exception as e:
@@ -33,41 +35,35 @@ def findMasterBankById(masterBankId:str)->tuple[TypeMasterBank, int]:
     return {**masterBankData, '_id': str(masterBankData['_id'])}, 200
 
 def insertMasterBank(masterBankInput:TypeMasterBankInput)->tuple[TypeMasterBank, int]:
-    # validate required field
-    try: 
-        masterBankData = {
-            '_id': masterBankInput['name'].lower(),
-            'name': masterBankInput['name'],
-            'bankDesc': masterBankInput['bankDesc'].lower(),
-            'activeStatus': True,
-            'setup': {
-                'createDate': datetime.now(UTC),
-                'updateDate': datetime.now(UTC),
-                # TODO: change to user data
-                'createUser': 'SYSTEM',
-                'updateUser': 'SYSTEM'
-            }
+    masterBankData = {
+        'name': masterBankInput['name'],
+        'bankDesc': masterBankInput['bankDesc'].lower(),
+        'activeStatus': True,
+        'setup': {
+            'createDate': datetime.now(UTC),
+            'updateDate': datetime.now(UTC),
+            # TODO: change to user data
+            'createUser': 'SYSTEM',
+            'updateUser': 'SYSTEM'
         }
-    except KeyError as e:
-        abort(422, f'Missing required field: {str(e)}')
-    except Exception as e:
-        abort(500, str(e))
+    }
 
     # insert master bank data
     try:
-        masterBankCollection.insert_one(masterBankData)
-    except DuplicateKeyError as e:
-        abort(409, str(e))
+        response = masterBankCollection.insert_one(masterBankData)
+    except WriteError as e:
+        error_message = e.details.get('errmsg', str(e))
+        abort(422, error_message)
     except Exception as e:
         abort(500, str(e))
 
-    return masterBankData, 201
+    return {**masterBankData, '_id':str(response.inserted_id)}, 201
 
 def updateMasterBank(masterBankId:str, masterBankInput:TypeMasterBankInput)->tuple[TypeMasterBank, int]:
     # check if master bank data exists
     try:
         masterBankData = masterBankCollection.find_one({
-            '_id': masterBankId,
+            '_id': ObjectId(masterBankId),
             'activeStatus': True
         })
     except Exception as e:
@@ -76,41 +72,35 @@ def updateMasterBank(masterBankId:str, masterBankInput:TypeMasterBankInput)->tup
         abort(404, 'Master Bank Data not Found')
 
     # prepare data to update
-    try:
-        masterBankDataUpdated = {
-            **masterBankData,
-            '_id': masterBankId,
-            'name': masterBankInput['name'],
-            'bankDesc': masterBankInput['bankDesc'].lower(),
-            'setup': {
-                **masterBankData['setup'],
-                'updateDate': datetime.now(UTC),
-                # TODO: change to user data
-                'updateUser': 'SYSTEM'
-            }
+    masterBankData = {
+        **masterBankData,
+        'name': masterBankInput['name'],
+        'bankDesc': masterBankInput['bankDesc'].lower(),
+        'setup': {
+            **masterBankData['setup'],
+            'updateDate': datetime.now(UTC),
+            # TODO: change to user data
+            'updateUser': 'SYSTEM'
         }
-    except KeyError as e:
-        abort(422, f'Missing required field: {str(e)}')
-    except Exception as e:
-        abort(500, str(e))
+    }
 
     # update master bank data
     try:
-        masterBankCollection.update_one({
-            '_id': masterBankId
+        masterBankDataUpdated = masterBankCollection.find_one_and_update({
+            '_id': ObjectId(masterBankId)
         }, {
-            '$set': masterBankDataUpdated
-        })
+            '$set': masterBankData
+        }, return_document=True)
     except Exception as e:
         abort(500, str(e))
 
-    return masterBankDataUpdated, 200
+    return {**masterBankDataUpdated, '_id': str(masterBankDataUpdated['_id'])}, 200
 
 def removeMasterBank(masterBankId:str)->tuple[None, int]:
     # check if master bank data exists
     try:
         masterBankData = masterBankCollection.find_one({
-            '_id': masterBankId,
+            '_id': ObjectId(masterBankId),
             'activeStatus': True
         })
     except Exception as e:
@@ -121,7 +111,7 @@ def removeMasterBank(masterBankId:str)->tuple[None, int]:
     # update master bank data
     try:
         masterBankCollection.update_one({
-            '_id': masterBankId
+            '_id': ObjectId(masterBankId)
         }, {
             '$set': {
                 'activeStatus': False,
