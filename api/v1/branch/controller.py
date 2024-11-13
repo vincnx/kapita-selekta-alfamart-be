5fd2041ac7183ffb27f2e9afd1b2dd7cc9b503bf
vincent exelcio <vincent.celcio@gmail.com>
2024-11-13 11:38:32 +0700

    feat: add branch API endpoint to get branch by user session - GET /v1/branch/user

--- api/v1/branch/controller.py
+++ api/v1/branch/controller.py
@@ -1,6 +1,7 @@
 from typing import List
 from bson import ObjectId
-from flask import abort
+from flask import abort, g
+from api.v1.middlewares.verifyRole import verifyRole
 from common.db import dbInstance
 from common.helpers.types import TypeBranch
 from bson.errors import InvalidId
@@ -24,6 +25,25 @@ def findBranchById(branchId: str) -> tuple[dict[str, TypeBranch], int]:
     try:
         branchData = branchCollection.find_one({
             '_id': ObjectId(branchId),
+        })
+    except InvalidId:
+        abort(422, 'Invalid Branch ID')
+    except Exception as e:
+        abort(500, str(e))
+    if not branchData:
+        abort(404, 'Branch Data Not Found')
+
+    return {
+        'data': {**branchData, '_id': str(branchData['_id'])}
+    }, 200
+
+@verifyRole(['branch'])
+def findBranchByUser() -> tuple[dict[str, TypeBranch], int]:
+    userData = g.user
+
+    try:
+        branchData = branchCollection.find_one({
+            '_id': ObjectId(userData['branch']['branchId'])
         })
     except InvalidId:
         abort(422, 'Invalid Branch ID')
--- api/v1/branch/routes.py
+++ api/v1/branch/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 
-from api.v1.branch.controller import findAllBranch, findBranchById
+from api.v1.branch.controller import findAllBranch, findBranchById, findBranchByUser
 
 
 branchRoutes = Blueprint('branchRoutes', __name__, url_prefix='/v1/branch')
@@ -13,4 +13,9 @@ def getAllBranch():
 @branchRoutes.route('/<string:branchId>', methods=['GET'])
 def getBranchById(branchId: str):
     data, status = findBranchById(branchId)
+    return jsonify(data), status
+
+@branchRoutes.route('/user', methods=['GET'])
+def getBranchByUser():
+    data, status = findBranchByUser()
     return jsonify(data), status
