aeb5280469fb8205d376e93c3626e5b3fe9d29ec
vincent exelcio <vincent.celcio@gmail.com>
2024-11-21 13:19:47 +0700

    fix: change config.py to get redis url from .env

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 class Config:
     SESSION_TYPE = 'redis'
-    SESSION_REDIS = 'redis://localhost:6379'
+    SESSION_REDIS = os.getenv('REDIS_URL')
     SESSION_PERMANENT = False
     PERMANENT_SESSION_LIFETIME = 60 * 60
     
